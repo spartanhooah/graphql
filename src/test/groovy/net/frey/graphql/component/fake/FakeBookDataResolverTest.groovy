433b9b97f4a457b5e9c7a524656d8aad0726a22e
Ben <spartanhooah@gmail.com>
2024-04-14 09:15:35 -0400

    add more assertions to book query test

--- src/test/groovy/net/frey/graphql/component/fake/FakeBookDataResolverTest.groovy
+++ src/test/groovy/net/frey/graphql/component/fake/FakeBookDataResolverTest.groovy
@@ -1,10 +1,12 @@
 package net.frey.graphql.component.fake
 
+import com.jayway.jsonpath.TypeRef
 import com.netflix.graphql.dgs.DgsQueryExecutor
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest
 import net.datafaker.Faker
 import net.frey.graphql.generated.client.BooksGraphQLQuery
 import net.frey.graphql.generated.client.BooksProjectionRoot
+import net.frey.graphql.generated.types.Author
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
@@ -31,9 +33,29 @@ class FakeBookDataResolverTest extends Specification {
         def request = new GraphQLQueryRequest(query, projectionRoot).serialize()
 
         when:
-        def titles = queryExecutor.executeAndExtractJsonPath(query, "data.books[*].title")
+        def titles = queryExecutor.executeAndExtractJsonPath(request, "data.books[*].title")
 
         then:
         titles.size() > 0
+
+        and:
+        when:
+        def authors = queryExecutor.executeAndExtractJsonPathAsObject(
+            request,
+            "data.books[*].author",
+            new TypeRef<List<Author>>(){})
+
+        then:
+        titles.size() == authors.size()
+
+        and:
+        when:
+        def releaseYears = queryExecutor.executeAndExtractJsonPathAsObject(
+            request,
+            "data.books[*].released.year",
+            new TypeRef<List<Integer>>() {})
+
+        then:
+        releaseYears.size() == titles.size()
     }
 }
