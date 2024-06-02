93c1a6e91428f22a11cf42240aa3770388d5296d
spartanhooah <spartanhooah@gmail.com>
2024-06-02 09:09:28 -0400

    add Hibernate dialect to try to fix connection on Linux

--- src/main/java/net/frey/graphql/client/StarWarsGraphqlClient.java
+++ src/main/java/net/frey/graphql/client/StarWarsGraphqlClient.java
@@ -4,6 +4,9 @@ import com.jayway.jsonpath.TypeRef;
 import com.netflix.graphql.dgs.client.GraphQLError;
 import com.netflix.graphql.dgs.client.GraphQLResponse;
 import com.netflix.graphql.dgs.client.WebClientGraphQLClient;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import net.frey.graphql.client.response.FilmResponse;
 import net.frey.graphql.client.response.PlanetResponse;
 import net.frey.graphql.client.response.StarshipResponse;
@@ -11,14 +14,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Component
 public class StarWarsGraphqlClient {
     private static final String URL = "https://swapi-graphql.netlify.app/.netlify/functions/index";
-    private static final String QUERY = """
+    private static final String QUERY =
+            """
         query allPlanets {
           allPlanets {
             planets {
@@ -51,28 +51,27 @@ public class StarWarsGraphqlClient {
     }
 
     public Mono<String> asJson(String operation, Map<String, Object> variables) {
-        return getGraphqlResponse(operation, variables)
-            .map(GraphQLResponse::getJson);
+        return getGraphqlResponse(operation, variables).map(GraphQLResponse::getJson);
     }
 
     public Mono<List<PlanetResponse>> allPlanets() {
         return getGraphqlResponse("allPlanets", null)
-            .map( response -> response.extractValueAsObject("allPlanets.planets", new TypeRef<>() {}));
+                .map(response -> response.extractValueAsObject("allPlanets.planets", new TypeRef<>() {}));
     }
 
     public Mono<StarshipResponse> oneStarshipFixed() {
         return getGraphqlResponse("oneStarshipFixed", null)
-            .map(response -> response.extractValueAsObject("starship", new TypeRef<>() {}));
+                .map(response -> response.extractValueAsObject("starship", new TypeRef<>() {}));
     }
 
     public Mono<FilmResponse> oneFilm(String filmId) {
         return getGraphqlResponse("oneFilm", Map.of("filmId", filmId))
-            .map(response -> response.extractValueAsObject("starship", FilmResponse.class));
+                .map(response -> response.extractValueAsObject("starship", FilmResponse.class));
     }
 
     public Mono<List<GraphQLError>> oneFilmInvalid() {
         return getGraphqlResponse("oneFilm", Map.of("filmId", "xxxx"))
-            .map(response -> response.hasErrors() ? response.getErrors() : null);
+                .map(response -> response.hasErrors() ? response.getErrors() : null);
     }
 
     private Mono<GraphQLResponse> getGraphqlResponse(String operation, Map<String, ?> variables) {
